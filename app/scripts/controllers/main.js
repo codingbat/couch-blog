401499f935a995481c696664b15148d612ea20c4
Nazmul Alam <Nazmul.Alam@students.ittralee.ie>
2016-10-12 22:38:28 +0100

    created skeleton code with yeoman

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name couchBlogApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the couchBlogApp
+ */
+angular.module('couchBlogApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
